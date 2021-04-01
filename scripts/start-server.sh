86cf7e0b5494197f0f4a92ac9e033195942c373a
ich777 <ich777@noreply.example.org>
2021-04-01 17:50:48 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export DISPLAY=:99
+export XAUTHORITY=${DATA_DIR}/.Xauthority
 
 LAT_V="$(wget -qO- https://github.com/ich777/versions/raw/master/FerdiClient | grep LATEST | cut -d '=' -f2)"
 CUR_V="$(find ${DATA_DIR} -name "ferdiclient-*" | cut -d '-' -f2)"
@@ -84,16 +85,15 @@ echo "---Checking for old logfiles---"
 find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
 find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
 echo "---Checking for old display lock files---"
-find /tmp -name ".X99*" -exec rm -f {} \; > /dev/null 2>&1
+rm -rf /tmp/.X99*
+rm -rf /tmp/.X11*
+rm -rf ${DATA_DIR}/.vnc/*.log
 screen -wipe 2&>/dev/null
 
 chmod -R ${DATA_PERM} ${DATA_DIR}
 
-echo "---Starting Xvfb server---"
-screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
-sleep 2
-echo "---Starting x11vnc server---"
-screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
+echo "---Starting TurboVNC server---"
+vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :99 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
 sleep 2
 echo "---Starting Fluxbox---"
 screen -d -m env HOME=/etc /usr/bin/fluxbox
